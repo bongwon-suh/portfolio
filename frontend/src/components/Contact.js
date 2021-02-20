b5e6d2d13851a4ecc483d098b2c15a9f3e1e86cc
bongwon-suh <suhliebe@gmail.com>
2021-02-21 04:27:23 +0900

    add contact component

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Contact = () => (
+  <section>
+    <Container className="mt-5 pt-5 mb-5 pb-5">
+      <h1 className="mb-4">Contact</h1>
+      <p>
+        안녕하세요. 서봉원입니다.
+        <br />
+        개발자가 되고 싶은 서봉원입니다. 안녕하세요. 서봉원입니다.
+        <br />
+        안녕하세요. 서봉asdsadasdsadasdasdasdasdasdasd원입니다.
+        <br />
+      </p>
+    </Container>
+  </section>
+);
+
+export default Contact;
