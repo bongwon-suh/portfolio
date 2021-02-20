7e95cf381bf767f0acba573d374fe4c6b6eb120d
bongwon-suh <suhliebe@gmail.com>
2021-02-21 04:29:51 +0900

    add summary component

---
+++ frontend/src/components/Summary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Summary = () => (
+    <section>
+      <Container className="mt-5 pt-5">
+        <h1 className="mb-4">SUMMARY</h1>
+        <p>
+          안녕하세요. 서봉원입니다.
+          <br />
+          개발자가 되고 싶은 서봉원입니다. 안녕하세요. 서봉원입니다.
+          <br />
+          안녕하세요. 서봉asdsadasdsadasdasdasdasdasdasd원입니다.
+          <br />
+        </p>
+      </Container>
+    </section>
+);
+
+export default Summary;
