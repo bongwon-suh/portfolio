bb81f882fc0f73292cd01d8605a66ccedfc8fe0e
bongwon-suh <suhliebe@gmail.com>
2021-02-23 23:13:01 +0900

    add footer

--- frontend/src/hocs/Layout.js
+++ frontend/src/hocs/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Layout.css'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const layout = (props) => (
-    <div>
+    <div className='main-layout'>
         <Navbar />
         {props.children}
         <Footer />
